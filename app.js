74ae94496a79da7557290c7934720d4229534f12
Ademola Sobaki <demolasobaki@gmail.com>
2020-07-26 11:17:09 +0100

    create add user and details page and route

--- app.js
+++ app.js
@@ -34,6 +34,7 @@ app.get('/', (req, res, next) => {
     res.render('searchusers')
 })
 
+
 // Search processing
 app.post('/user/search', (req, res, next) => {
     let id = req.body.id;
@@ -52,6 +53,40 @@ app.post('/user/search', (req, res, next) => {
     })
 })
 
+// Add User Page
+app.get('/user/add', (req, res, next) => {
+    res.render('adduser')
+})
+
+// Process Add user page
+app.post('/user/add', (req, res, next) => {
+
+    let id = req.body.id;
+    let first_name = req.body.first_name;
+    let last_name = req.body.last_name;
+    let email = req.body.email;
+    let phone = req.body.phone;
+
+    client.hmset(id, [
+        'first_name', first_name,
+        'last_name', last_name,
+        'email', email,
+        'phone', phone
+    ], (err, reply) => {
+        if (err) {
+            console.log(err)
+        }
+        console.log('reply', reply)
+        res.redirect('/')
+    })
+})
+
+
+// Delete User
+app.delete('/user/delete/:id', (req, res, next) => {
+    client.del(req.params.id)
+    res.redirect('/')
+})
 app.listen(PORT, () => {
     console.log(`Server started on PORT: ${PORT}`)
 })
