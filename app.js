5fb9e882d27acafa0e1884ca6185e318e34e5ff6
Ademola Sobaki <demolasobaki@gmail.com>
2020-07-26 01:15:06 +0100

    connect to redis instance, create user views

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const redis = require('redis')
 
+// Create Redis CLient
+let client = redis.createClient()
+
+client.on('connect', () => {
+    console.log(`Connected to Redis`)
+})
+
 // Set Port
 const PORT = 3000;
 
@@ -22,8 +29,27 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // method override
 app.use(methodOverride('_method'))
 
+// Search page
 app.get('/', (req, res, next) => {
     res.render('searchusers')
+})
+
+// Search processing
+app.post('/user/search', (req, res, next) => {
+    let id = req.body.id;
+
+    client.hgetall(id, (err, obj) => {
+        if (!obj) {
+            res.render('searchusers', {
+                error: "User does not exist"
+            })
+        } else {
+            obj.id = id
+            res.render('details', {
+                user: obj
+            })
+        }
+    })
 })
 
 app.listen(PORT, () => {
